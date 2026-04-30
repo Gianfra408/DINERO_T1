c573a1b5819d414007101d3d7710d4e64bb8af72
Gianfranco <n00507122@upn.pe>
2026-04-29 21:24:30 -0500

    SELECCION

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+          double SALDO = 8000.0;
 
             Console.WriteLine("---- CAJERO AUTOMÁTICO ----");
             Console.WriteLine("1. CONSULTAR SALDO");
@@ -11,5 +12,13 @@ class Program
             Console.WriteLine("3. RETIRAR DINERO");
             Console.WriteLine("4. SALIR");
             Console.Write("SELECCIONE UNA OPCION: ");
+            
+          int OPCION = int.Parse(Console.ReadLine());
+          
+          if (OPCION > 4 || OPCION < 0)
+    {
+      Console.WriteLine("OPCION INVALIDA");
+    }
+    
     }
 }
