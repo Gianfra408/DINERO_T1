4a60dd1ae7cc5f51f2939c6a554c79c5e342079e
Gianfranco <n00507122@upn.pe>
2026-04-29 21:13:55 -0500

    OPCIONES

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+            Console.WriteLine("---- CAJERO AUTOMÁTICO ----");
+            Console.WriteLine("1. CONSULTAR SALDO");
+            Console.WriteLine("2. DEPOSITAR DINERO");
+            Console.WriteLine("3. RETIRAR DINERO");
+            Console.WriteLine("4. SALIR");
+            Console.Write("SELECCIONE UNA OPCION: ");
+    }
+}
